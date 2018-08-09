c1ce492b51c11fbc419c78c78d6c97460adbdb18
Haider Malik <haidermalik504@gmail.com>
2018-08-09 13:06:38 +0500

    add forms-validations

--- src/components/FormControl/TextInput.js
+++ src/components/FormControl/TextInput.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const TextInput = ({ label, type, name, placeholder, value, onChange }) => {
+const TextInput = ({
+  label,
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  error
+}) => {
   return (
     <div className="form-group">
       <label htmlFor="title">{label}</label>
       <input
         type={type}
-        className="form-control"
+        className={classnames('form-control', {
+          //if there is any value inside error then add the is-invalid class
+          'is-invalid': error
+        })}
         name={name}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
       />
+      {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };

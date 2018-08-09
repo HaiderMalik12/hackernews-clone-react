3f5bd8878b166b4213b23a67041f1da5d0374714
Haider Malik <haidermalik504@gmail.com>
2018-08-09 16:42:21 +0500

    delete comments

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ import Links from './components/Link/Links';
 import { AddLink } from './components/Link/AddLink';
 
 class App extends Component {
-  // deleteLink = id => {
-  //   API.deleteLink(id)
-  //     .then(response => {
-  //       const newLinks = this.state.links.filter(link => link.id !== id);
-  //       this.setState({ links: newLinks });
-  //     })
-  //     .catch(err => {
-  //       this.setState(() => ({
-  //         error: err
-  //       }));
-  //     });
-  // };
   render() {
     return (
       <AppProvider>

d46e5e009aa11779b40a488c12d146c8dd457b7b
Haider Malik <haidermalik504@gmail.com>
2018-08-11 09:28:43 +0500

    removed store

--- src/store/action_types.js
+++
@@ -1,4 +0,0 @@
-export const DELETE_LINK_TYPE = 'DELETE_LINK_TYPE';
-export const ADD_LINK_TYPE = 'ADD_LINK_TYPE';
-export const EDIT_LINK_TYPE = 'EDIT_LINK_TYPE';
-export const ERROR_TYPE = 'ERROR';

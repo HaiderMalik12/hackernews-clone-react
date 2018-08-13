90b583b55cf70a0b08f6152be7404ecae0ea3075
Haider Malik <haidermalik504@gmail.com>
2018-08-13 08:41:43 +0500

    create deleteAsync action

--- src/store/actions/linkActions.js
+++ src/store/actions/linkActions.js
@@ -2,18 +2,18 @@ import { GET_LINKS, DELETE_LINK, ADD_LINK } from './types';
 import * as API from '../../shared/http';
 
 export const getLinks = () => async dispatch => {
-  //fetch all the links from the API
   const { data } = await API.fetchLinks();
   dispatch({
     type: GET_LINKS,
     payload: data
   });
 };
-export const deleteLink = id => {
-  return {
+export const deleteLink = id => async dispatch => {
+  await API.deleteLink(id);
+  dispatch({
     type: DELETE_LINK,
     payload: id
-  };
+  });
 };
 export const addLink = link => {
   return {
